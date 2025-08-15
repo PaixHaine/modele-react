2efc662779f86449982e3e214db6bcdb452e1d93
PaixHaine <pnzinsou@gmail.com>
2025-08-15 03:25:15 +0200

    feat(Styles): même rendu BO et Front & utilisation styles Gutenberg natifs

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,21 +2,41 @@ import React from 'react';
 import Head from 'next/head';
 import tachyons from 'tachyons/css/tachyons.min.css';
 import stylesheet from '../src/styles/style.scss';
+import Config from '../config';
 
-const Header = () => (
-  <Head>
-    <style
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: tachyons }}
-    />
-    <style
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: stylesheet }}
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="utf-8" />
-    <title>WordPress + React Starter Kit Frontend by Postlight</title>
-  </Head>
-);
+const Header = () => {
+  // URLs des styles Gutenberg
+  const gutenbergStyles = [
+    `${Config.apiUrl.replace('/wp-json', '')}/wp-includes/css/dist/block-library/style.min.css`,
+    `${Config.apiUrl.replace('/wp-json', '')}/wp-includes/css/dist/block-library/theme.min.css`,
+    `${Config.apiUrl.replace('/wp-json', '')}/wp-includes/css/dist/block-editor/style.min.css`,
+    `${Config.apiUrl.replace('/wp-json', '')}/wp-includes/css/dist/format-library/style.min.css`,
+  ];
+
+  return (
+    <Head>
+      <style
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: tachyons }}
+      />
+      <style
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: stylesheet }}
+      />
+      {/* Styles de Gutenberg */}
+      {gutenbergStyles.map((styleUrl, index) => (
+        <link 
+          key={`gutenberg-${index}`}
+          rel="stylesheet" 
+          href={styleUrl}
+          media="all"
+        />
+      ))}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <title>WordPress + React Starter Kit Frontend by Postlight</title>
+    </Head>
+  );
+};
 
 export default Header;
--- wordpress/wp-content/themes/postlight-headless-wp/functions.php
+++ wordpress/wp-content/themes/postlight-headless-wp/functions.php
@@ -31,3 +31,102 @@ require_once 'inc/acf-options.php';
 
 // Add GraphQL resolvers.
 require_once 'inc/graphql/resolvers.php';
+
+/**
+ * Charger les styles natifs de Gutenberg
+ */
+function postlight_headless_gutenberg_styles() {
+    // Charger les styles de base des blocs Gutenberg
+    wp_enqueue_style( 'wp-block-library' );
+    
+    // Charger les styles spécifiques au thème si supportés
+    if ( current_theme_supports( 'wp-block-styles' ) ) {
+        wp_enqueue_style( 'wp-block-library-theme' );
+    }
+    
+    // Charger les styles d'édition pour l'admin
+    if ( is_admin() ) {
+        wp_enqueue_style( 'wp-edit-blocks' );
+    }
+}
+add_action( 'enqueue_block_assets', 'postlight_headless_gutenberg_styles' );
+
+/**
+ * Activer le support des styles de blocs
+ */
+function postlight_headless_theme_supports() {
+    // Activer le support des styles de blocs Gutenberg
+    add_theme_support( 'wp-block-styles' );
+    
+    // Activer le support des couleurs de blocs
+    add_theme_support( 'editor-color-palette' );
+    
+    // Activer le support de la typographie
+    add_theme_support( 'editor-font-sizes' );
+    
+    // Activer le support des alignements larges
+    add_theme_support( 'align-wide' );
+}
+add_action( 'after_setup_theme', 'postlight_headless_theme_supports' );
+
+/**
+ * Endpoint REST API pour les styles de Gutenberg
+ */
+function postlight_headless_gutenberg_styles_endpoint() {
+    register_rest_route( 'postlight-headless/v1', '/gutenberg-styles', array(
+        'methods' => 'GET',
+        'callback' => 'postlight_headless_get_gutenberg_styles',
+        'permission_callback' => '__return_true',
+    ) );
+}
+add_action( 'rest_api_init', 'postlight_headless_gutenberg_styles_endpoint' );
+
+/**
+ * Récupérer les styles de Gutenberg
+ */
+function postlight_headless_get_gutenberg_styles() {
+    // Forcer le chargement des styles de Gutenberg
+    wp_enqueue_style( 'wp-block-library' );
+    wp_enqueue_style( 'wp-block-library-theme' );
+    wp_enqueue_style( 'wp-edit-blocks' );
+    wp_enqueue_style( 'wp-format-library' );
+    
+    // Récupérer les URLs des styles
+    $styles = array();
+    
+    // Styles de base des blocs
+    $block_library_url = includes_url( 'css/dist/block-library/style.min.css' );
+    $styles['block-library'] = $block_library_url;
+    
+    // Styles du thème
+    $block_theme_url = includes_url( 'css/dist/block-library/theme.min.css' );
+    $styles['block-theme'] = $block_theme_url;
+    
+    // Styles d'édition
+    $block_editor_url = includes_url( 'css/dist/block-editor/style.min.css' );
+    $styles['block-editor'] = $block_editor_url;
+    
+    // Styles de la bibliothèque de formats
+    $format_library_url = includes_url( 'css/dist/format-library/style.min.css' );
+    $styles['format-library'] = $format_library_url;
+    
+    // Styles globaux WordPress
+    $global_styles_url = includes_url( 'css/dist/global-styles/style.min.css' );
+    $styles['global-styles'] = $global_styles_url;
+    
+    return array(
+        'styles' => $styles,
+        'css_urls' => array(
+            $block_library_url,
+            $block_theme_url,
+            $block_editor_url,
+            $format_library_url,
+            $global_styles_url
+        ),
+        'info' => array(
+            'description' => 'Tous les styles Gutenberg nécessaires pour le frontend',
+            'version' => get_bloginfo('version'),
+            'theme' => get_template()
+        )
+    );
+}
